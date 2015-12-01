939e473bc8109fb8f67359f55a1434aa619c53f3
broszhu <ooxx@zhu.me>
2015-12-01 23:43:49 +0800

    addd

---
+++ 1-4/13.http/buffer.js
@@ -0,0 +1,8 @@
+var buf1=new Buffer(12);
+console.log(buf1);
+buf1.fill(01,0,4);
+console.log(buf1);
+var buf1=new Buffer([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
+console.log(buf1);
+var buf1=new Buffer([26,27,28,29,30]);
+console.log(buf1);
